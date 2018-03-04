d0b39a34168789a7b989ab7b49a8e3d2bf3fa080
Ross Wintle <ross@oikos.org.uk>
2018-03-04 23:03:05 +0000

    Display referring domain

--- resources/views/site/show.blade.php
+++ resources/views/site/show.blade.php
@@ -1,6 +1,10 @@
 <h3>Showing {{ $site->views->count() }} views for {{ $site->domain }}</h3>
 <ul>
 @foreach( $site->views as $view )
-    <li>Time: {{ $view->created_at }} Page: {{ $view->page ? $view->page->url : 'None' }}, UA: {{ $view->user_agent->name }}</li>
+    <li>Time: {{ $view->created_at }}
+        Page: {{ $view->page ? $view->page->url : 'None' }},
+        UA: {{ $view->user_agent->name }},
+        Referrer: {{ $view->referring_domain ? $view->referring_domain->domain : 'None' }}
+    </li>
 @endforeach
 </ul>
