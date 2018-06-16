8101a37056418a116692700e8d3e6ac30dae7102
Ross Wintle <ross@oikos.org.uk>
2018-06-16 07:46:41 +0100

    Tidy welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Kownter</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -42,11 +42,17 @@
             }
 
             .content {
+                width: 100%;
+                max-width: 720px;
                 text-align: center;
             }
 
             .title {
                 font-size: 84px;
+            }
+
+            .subtitle {
+                font-size: 30px;
             }
 
             .links > a {
@@ -78,15 +84,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kownter
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="subtitle">
+                    Simple, private, self-hosted, cookie free website analytics...one day...maybe
                 </div>
             </div>
         </div>
